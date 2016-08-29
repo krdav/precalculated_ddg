aebaa4b3c547d33c4df1d1c66140840a859352c3
krdav <krdav@cbs.dtu.dk>
2016-08-29 23:41:46 +0200

    g

--- delete_traj_files.py
+++ delete_traj_files.py
@@ -1,25 +1,29 @@
 import glob
 import multiprocessing
 import os
-
+import sys
 
 def delete_traj(split):
-    prot_glob = split + '/' + '*/'
+    # print(split)
+    prot_glob = split + '*/'
     prots = glob.glob(prot_glob)
     for prot in prots:
-        chain_glob = prot + '/' + '*/'
+        chain_glob = prot + '*/'
         chains = glob.glob(chain_glob)
         for chain in chains:
-            traj_glob = chain + '/' + 'mutant_traj*'
+            # print(chain)
+            traj_glob = chain + 'mutant_traj*'
             trajs = glob.glob(traj_glob)
             for traj in trajs:
-                print(traj)
-                # os.remove(traj)
+                # print(traj)
+                os.remove(traj)
 
 
-split_dir = '/home/projects/cu_10020/data/precalculated_ddg/prot_list.txt/split'
+split_dir = '/home/projects/cu_10020/data/precalculated_ddg/split'
 splits_glob = split_dir + '/' + '*/'
 splits = glob.glob(splits_glob)
 
+# print(splits)
+# sys.exit()
 pool = multiprocessing.Pool(28)
 output = pool.map(delete_traj, splits)
