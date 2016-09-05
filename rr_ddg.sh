f61eff9e345d5ba185f232d0269979e0377fd700
krdav <krdav@cbs.dtu.dk>
2016-09-05 08:20:36 +0200

    h

--- rr_ddg.sh
+++ rr_ddg.sh
@@ -1,4 +1,7 @@
 
-#python run_ddg.py --check_all_folders 1
-python run_ddg.py --folders_for_ddg prot_list.txt -v 1
+python run_ddg.py --check_all_folders 1
+# python run_ddg.py --folders_for_ddg prot_list.txt -v 1
+# python run_ddg.py --folders_for_ddg folders_for_ddg.txt -v 1
+
+
 
